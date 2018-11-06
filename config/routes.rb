041e4e5973f1acfca45fd3d4abc0c0a634dc3bb0
Vanhoz <vanhoz507@gmail.com>
2018-11-06 14:20:36 +0300

    main + candidate

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'high_voltage/pages#show', id: 'main'
+  get '/candidate' => 'high_voltage/pages#show', id: 'candidate'
+end
